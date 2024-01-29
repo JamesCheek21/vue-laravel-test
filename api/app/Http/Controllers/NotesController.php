9f2ed728a9598bc6b28981aa04919ebc37ad19b6
JamesCheek21 <james.cheek@twine.net>
2024-01-29 20:01:57 +0000

    Added migration, route, model, controller and validation requests for notes

---
+++ api/app/Http/Controllers/NotesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notes;
+use App\Http\Requests\NotesStoreRequest;
+use App\Http\Requests\NotesUpdateRequest;
+use Illuminate\Http\Request;
+
+class NotesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $notes = Notes::all();
+        return response()->json($notes);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(NotesStoreRequest $request)
+    {
+        $note = Notes::create([
+            'title' => $request->input['title'],
+            'content' => $$request->input['content'],
+        ]);
+
+        return response()->json($note, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $note = Notes::findOrFail($id);
+        return response()->json($note);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(NotesUpdateRequest $request, string $id)
+    {
+        $data = $request->validated();
+
+        $note = Notes::findOrFail($id);
+        $note->update($data);
+
+        return response()->json($note);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $note = Notes::findOrFail($id);
+        $note->delete();
+
+        return response()->json(['message' => 'Note deleted successfully']);
+    }
+}
