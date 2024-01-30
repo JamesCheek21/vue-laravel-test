48db0509208977bee018a946cc00b1511edb298e
JamesCheek21 <james.cheek@twine.net>
2024-01-30 20:16:51 +0000

    View Page

---
+++ frontend/src/views/NoteView.vue
@@ -0,0 +1,89 @@
+<template>
+  <main class="w-5/6 md:w-3/4 lg:w-1/2">
+    <div class="py-5 my-6 border-b-2 flex justify-between items-center">
+      <RouterLink to="/" class="hover:underline flex items-center gap-2">
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.4"
+            d="M5 12h14M5 12l4-4m-4 4 4 4"
+          />
+        </svg>
+        Home Page
+      </RouterLink>
+      <div class="flex items-center gap-2">
+        <RouterLink
+          :to="'/edit/' + note.id"
+          class="flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          <svg
+            class="w-6 h-6 text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.4"
+              d="m14.3 4.8 2.9 2.9M7 7H4a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h11c.6 0 1-.4 1-1v-4.5m2.4-10a2 2 0 0 1 0 3l-6.8 6.8L8 14l.7-3.6 6.9-6.8a2 2 0 0 1 2.8 0Z"
+            />
+          </svg>
+          Edit
+        </RouterLink>
+        <button
+          type="button"
+          class="flex items-center gap-2 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        >
+          <svg
+            class="w-6 h-6 text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.4"
+              d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+            />
+          </svg>
+          Delete
+        </button>
+      </div>
+    </div>
+    <p class="text-3xl font-bold mb-4">{{ note.title }}</p>
+    <p class="text-xl">{{ note.content }}</p>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+import { RouterLink } from 'vue-router'
+
+export default {
+  data() {
+    return {
+      note: {},
+    }
+  },
+  async created() {
+    const noteId = this.$route.params.id
+    const { data } = await axios.get(`http://localhost/api/notes/${noteId}`)
+    this.note = data
+  },
+  components: { RouterLink }
+}
+</script>
